630e8a3e4f14a7bd4e9e30ecf3f6d95f86da8265
Yao <lucas952222@gmail.com>
2025-08-11 11:08:00 -0700

    chore

--- update_version.py
+++ update_version.py
@@ -1,26 +1,25 @@
-import re, os
-from setuptools import setup
+import os
+import re
 
-version_type = os.getenv('VERSION_TYPE')
+version_type = os.getenv("VERSION_TYPE")
 
-with open('setup.py', 'r') as f:
+with open("setup.py", "r") as f:
     setup_py = f.read()
 
 version = re.search(r"version='(\d+\.\d+\.\d+)'", setup_py).group(1)
-major, minor, patch = map(int, version.split('.'))
+major, minor, patch = map(int, version.split("."))
 
-if version_type == 'major':
+if version_type == "major":
     major += 1
     minor = 0
     patch = 0
-elif version_type == 'minor':
+elif version_type == "minor":
     minor += 1
     patch = 0
-elif version_type == 'patch':
+elif version_type == "patch":
     patch += 1
 
 new_version = f"{major}.{minor}.{patch}"
 
-with open('setup.py', 'w') as f:
+with open("setup.py", "w") as f:
     f.write(re.sub(r"version='(\d+\.\d+\.\d+)'", f"version='{new_version}'", setup_py))
-
